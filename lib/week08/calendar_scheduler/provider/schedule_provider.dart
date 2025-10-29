ebda3822c97c1c521f06a432ce60269387d3b887
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 00:50:59 +0900

    feat: Introduce ScheduleProvider with basic state management

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -6,3 +6,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
+class ScheduleProvider extends ChangeNotifier{
+  final ScheduleRepository repository;
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  Map<DateTime, List<ScheduleModel>> cache = {};
+
+  ScheduleProvider({
+    
+  })
+}
