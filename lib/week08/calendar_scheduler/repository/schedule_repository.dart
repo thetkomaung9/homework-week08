bfe8f061b00a406c538dd2034ccfdd336e58e4b9
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 02:11:52 +0900

    Fix: Corrected typo in schedule target URL
    The URL interpolation was missing a  sign.

--- lib/week08/calendar_scheduler/repository/schedule_repository.dart
+++ lib/week08/calendar_scheduler/repository/schedule_repository.dart
@@ -6,7 +6,7 @@ import 'package:dio/dio.dart';
 
 class ScheduleRepository {
   final _dio= Dio();
-  final _targetUrl = 'http://&{Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
+  final _targetUrl = 'http://${Platform.isAndroid ? '10.0.2.2' : 'localhost'}:3000/schedule';
 
   Future<List<ScheduleModel>> getSchedules({
     required DateTime date,
