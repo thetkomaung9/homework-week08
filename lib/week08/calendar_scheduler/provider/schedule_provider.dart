cd9e2443a8c52c9ea1ced075ccffcc004931e334
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 00:46:59 +0900

    feat: Implement basic ScheduleProvider structure
    Adds the basic structure for the ScheduleProvider, including imports and initial class definition.

---
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/rendering.dart';
+import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
+import 'package:myapp/week08/calendar_scheduler/repository/schedule_repository.dart';
+
+import '';
