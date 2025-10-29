8d8c6027c49f3d86fc93bb39a64db484e6042f01
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 00:38:27 +0900

    Fix: Format date string in ScheduleModel toJson
    Ensures consistent date format (YYYYMM) when serializing ScheduleModel to JSON.

--- lib/week08/calendar_scheduler/model/schedule_model.dart
+++ lib/week08/calendar_scheduler/model/schedule_model.dart
@@ -29,7 +29,7 @@ class ScheduleModel {
       return {
         'id' : id,
         'content' : content,
-        'date' :
+        'date' :'${date.year}${date.month.toString().padLeft(2, '0')}'
       }
     }
 }
