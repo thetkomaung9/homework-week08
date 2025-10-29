b4142ed92e8f765b7d3ded806e979f7e1ada2f09
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 00:47:27 +0900

    feat: Add provider dependencies and uuid package.

--- lib/week08/calendar_scheduler/provider/schedule_provider.dart
+++ lib/week08/calendar_scheduler/provider/schedule_provider.dart
@@ -2,4 +2,7 @@ import 'package:flutter/rendering.dart';
 import 'package:myapp/week08/calendar_scheduler/model/schedule_model.dart';
 import 'package:myapp/week08/calendar_scheduler/repository/schedule_repository.dart';
 
-import '';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+
