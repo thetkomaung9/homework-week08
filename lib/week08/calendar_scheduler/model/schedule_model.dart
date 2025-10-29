e4bb2219ff591526836c3d37d57e049ccbfe99b1
thetko1996 <thetkomaung966@gmail.com>
2025-10-30 00:31:57 +0900

    feat: Implement ScheduleModel
    This commit introduces the  class to represent schedule entries in the calendar scheduler.

--- lib/week08/calendar_scheduler/model/schedule_model.dart
+++ lib/week08/calendar_scheduler/model/schedule_model.dart
@@ -1,1 +1,13 @@
-cla
+class ScheduleModel {
+  final String id;
+  final String content;
+  final DateTime date;
+  final int startTime;
+  final int endTime;
+
+    ScheduleModel({
+      required this.id;
+      required this.content;
+      
+    })
+}
